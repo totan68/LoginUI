3755292a3debf3f0a2dba38e4bca374c25814130
totan68 <164209696+totan68@users.noreply.github.com>
2024-08-08 09:28:59 +0530

    Initial Commit

---
+++ LoginUI/LoginUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginUIApp.swift
+//  LoginUI
+//
+//  Created by Totan Hazra on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
